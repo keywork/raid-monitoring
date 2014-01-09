38459c30bbcceebc0a1897f3ba0e957cff4358fd
keywork <smahoney@constant.com>
2014-01-09 18:24:56 -0500

    Update softRaidSetup.sh

--- softRaidSetup.sh
+++ softRaidSetup.sh
@@ -3,7 +3,7 @@ rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.no
 yum install -y nagios-plugins-linux_raid 
 yum install -y nrpe
 echo "command[check_linux_raid]=/usr/lib64/nagios/plugins/check_linux_raid" >> /etc/nagios/nrpe.cfg
-sed -i 's/127.0.0.1/208.167.226.249/g' /etc/nagios/nrpe.cfg 
+sed -i 's/127.0.0.1/ENTER IP HERE/g' /etc/nagios/nrpe.cfg 
 sed -i 's/Defaults    requiretty/#Defaults  requiretty/g' /etc/sudoers 
 iptables -I INPUT -p tcp -m tcp --dport 5666 -j REJECT 
 service iptables save
